b18f34c524c0dc697afe81e09b87a752c12c1038
feyami <7311742+feyami@users.noreply.github.com>
2022-10-12 09:24:55 +0300

    auth route added

--- server/server.js
+++ server/server.js
@@ -5,12 +5,13 @@ import session from 'express-session';
 import passport from 'passport';
 import passportGoogle from './configuration/passportGoogleConfig.js';
 import MongoDbConnection from './configuration/mongoDbConfig.js';
-
+import authRoutes from './routes/auth.js';
 dotenv.config();
 const app = express();
+
 app.use(
   session({
-    secret: "codefcom",
+    secret: process.env.SECRET,
     resave: true,
     saveUninitialized: true,
 }));
@@ -18,13 +19,21 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 // app.use((req, res, next) => {
 //   res.header('Access-Control-Allow-Origin', '*')
 //   next()
 // })
+app.use("/auth", authRoutes);
+app.get("/", (req, res) => {
+  res.send("Hi");
+})
 
-
+app.get("/auth/logout", (req, res) => {
+  req.session.destroy(function (err) {
+    res.redirect('/login'); //Inside a callback… bulletproof!
+  });
+});
 
 MongoDbConnection();
 passportGoogle();
@@ -36,35 +45,3 @@ app.listen(port, () => {
 
 
 
-
-
-
-
-
-
-
-
- 
-
-app.get("/", (req, res) => {
-  res.send("Helllo WOlrd");
-})
-
-app.get("/getuser", (req, res) => {
-  res.send(req.user);
-})
-
-app.get("/auth/logout", (req, res) => {
-  if (req.user) {
-    req.logout();
-    res.send("done");
-  }
-})
-
-app.get("/auth/google", passport.authenticate("google", { scope: ["profile", "email"] }));
-
-app.get("/auth/google/callback", passport.authenticate("google", { failureRedirect: "http://localhost:3000", session: true }),
-  function (req, res) {
-    res.redirect('http://localhost:3000');
-  }
-);
